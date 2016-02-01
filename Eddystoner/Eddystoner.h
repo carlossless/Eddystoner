424a961f86a1394c79530c6296310354227ef02c
Karolis Stasaitis <stkarolis@gmail.com>
2016-02-01 20:04:59 +0100

    Initial commit

---
+++ Eddystoner/Eddystoner.h
@@ -0,0 +1,19 @@
+//
+//  Eddystoner.h
+//  Eddystoner
+//
+//  Created by Karolis Stasaitis on 31/01/16.
+//  Copyright © 2016 delanoir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for eddystoner.
+FOUNDATION_EXPORT double eddystonerVersionNumber;
+
+//! Project version string for eddystoner.
+FOUNDATION_EXPORT const unsigned char eddystonerVersionString[];
+
+#import <Eddystoner/ESSBeaconScanner.h>
+#import <Eddystoner/ESSEddystone.h>
+#import <Eddystoner/ESSTimer.h>
